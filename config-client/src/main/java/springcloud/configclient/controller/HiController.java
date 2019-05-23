b061d14f62eb691c0026da5c573b7fd217070a85
jay.zhu <jay.zhu@dadaabc.com>
2019-05-23 17:14:34 +0800

    分布式配置中心(Spring Cloud Config)

---
+++ config-client/src/main/java/springcloud/configclient/controller/HiController.java
@@ -0,0 +1,16 @@
+package springcloud.configclient.controller;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HiController {
+    @Value("${foo}")
+    String foo;
+
+    @RequestMapping(value = "/hi")
+    public String hi(){
+        return foo;
+    }
+}
